e1130ecabb5ada47e4ff98c4e252589e015fd390
Myroslav Gavryliak <myroslav.gavriliak@gmail.com>
2022-09-23 10:42:33 +0300

    refact(mapping): removed old comments

--- src/mapping.ts
+++ src/mapping.ts
@@ -8,48 +8,7 @@ import {
 } from "../generated/NFT/NFT"
 import { NFTToken, User } from "../generated/schema"
 
-export function handleApproval(event: Approval): void {
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract._tokenIds(...)
-  // - contract.balanceOf(...)
-  // - contract.getApproved(...)
-  // - contract.isApprovedForAll(...)
-  // - contract.minero(...)
-  // - contract.nTokenOnSale(...)
-  // - contract.name(...)
-  // - contract.ownerOf(...)
-  // - contract.supportsInterface(...)
-  // - contract.symbol(...)
-  // - contract.tokenByIndex(...)
-  // - contract.tokenOfOwnerByIndex(...)
-  // - contract.tokenURI(...)
-  // - contract.tokensData(...)
-  // - contract.totalSupply(...)
-  // - contract.minar(...)
-  // - contract.transferirNft(...)
-  // - contract.quemarNft(...)
-  // - contract.obtenerNfts(...)
-  // - contract.obtenerPaginav1(...)
-  // - contract.obtenerPaginav2(...)
-  // - contract.tokensOf(...)
-  // - contract.tokensOfPaginav1(...)
-  // - contract.getItemInfo(...)
-  // - contract.obtenerNftsbyrango(...)
-}
+export function handleApproval(event: Approval): void {}
 
 export function handleApprovalForAll(event: ApprovalForAll): void {}
 
